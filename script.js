4cffbabd1b884d11f70dcd90dfed2cf6fa30af00
Arun-Prasanth-P <arunmeenu1999@gmail.com>
2024-06-10 20:50:05 +0530

    Day 14-task

---
+++ script.js
@@ -0,0 +1,62 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '0' && currentOperand === '') return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentOperand;
+}
+
+
+clearDisplay();
